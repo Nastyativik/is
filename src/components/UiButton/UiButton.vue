c4b050653efaa1106bfa1c03efd5791a09bb61ee
Nastyativik <nastyativikru@gmail.com>
2025-12-23 13:35:11 +0300

    UiButton.vue

--- src/components/UiButton/UiButton.vue
+++ src/components/UiButton/UiButton.vue
@@ -16,7 +16,7 @@ import type { IProps, IEmit } from 'src/helpers';
 defineProps<IProps>();
 defineEmits<IEmit>();
 
-const layout = defineModel<string>('variant', { default: 'primary' });
+const layout = defineModel<string>('layout', { default: 'primary' });
 const isDisabled = defineModel<boolean>('isDisabled', { default: false });
 const type = defineModel<'button' | 'submit'>('type', { default: 'button' });
 </script>
